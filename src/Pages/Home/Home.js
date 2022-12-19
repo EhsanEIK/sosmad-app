0574ea0ec55ba14d600b184825191be3816ce57d
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-19 11:03:44 +0600

    added btn group under Cards component in Home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,6 +7,10 @@ const Home = () => {
         <div className='container mx-auto'>
             <Navbar></Navbar>
             <Cards></Cards>
+            <div className='mt-10 bg-gray-700 rounded-full p-2 md:mx-auto mx-3'>
+                <button className='btn bg-blue-600 border-blue-600 w-1/2'>Explore</button>
+                <button className='btn bg-transparent border-transparent w-1/2'>Discover</button>
+            </div>
         </div>
     );
 };
